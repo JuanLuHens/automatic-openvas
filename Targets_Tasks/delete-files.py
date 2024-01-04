378bdaa91a500e6383865cc77d7dfeddb864bc76
Juanlu <juanlu82@gmail.com>
2024-01-04 14:24:29 +0100

    remove logs and reports

--- Targets_Tasks/delete-files.py
+++ Targets_Tasks/delete-files.py
@@ -2,6 +2,7 @@ from gvm.connections import UnixSocketConnection
 from gvm.protocols.gmp import Gmp
 import xml.etree.ElementTree as ET
 import getpass
+import os
 
 def get_pass():
     password=getpass.getpass(prompt='Enter password: ')
@@ -18,12 +19,21 @@ with Gmp(connection=connection) as gmp:
     response = gmp.get_version()
     print(response)
     gmp.authenticate(user,password)
-    respuesta=gmp.get_port_lists()
+    respuesta = gmp.get_reports(filter_string='rows=1500')
     root = ET.fromstring(respuesta)
-    port_lists = root.findall('.//port_list')
-    for port_list in port_lists:
-        id = port_list.get('id')
-        name = port_list.find('name').text
-        print(f'ID: {id}')
-        print(f'Name: {name}')
-        print(f'------')
+    reports = root.findall(".//report")
+    for report in reports:
+        report_id = report.get("id")
+        task = report.find(".//task")
+        task_id = task.get("id")
+        task_name = task.find("name").text
+        print("Reporte a borrar")
+        print("Report ID:", report_id)
+        print("Task ID:", task_id)
+        print("Task Name:", task_name)
+        respuesta= gmp.delete_report(report_id)
+        print(respuesta)
+    if os.path.exists('/home/redteam/gvm/tasksend.txt'):
+        os.remove('/home/redteam/gvm/tasksend.txt')
+    if os.path.exists('/home/redteam/gvm/taskslog.txt'):
+        os.remove('/home/redteam/gvm/taskslog.txt')
