94093748ab717d0d0e03b498535b6156618cf6b6
Juanlu <juanlu82@gmail.com>
2024-01-17 12:15:34 +0100

    send email from auto update

--- Update/update-script.py
+++ Update/update-script.py
@@ -1,6 +1,34 @@
 import requests
 import json
 import subprocess
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def email(version, configuracion, resultado):
+    smtp_server = configuracion.get('mailserver')
+    smtp_port = 25  # Puerto 25 para autenticación anónima
+    from_address = configuracion.get('from')
+    to_address = configuracion.get('to')
+    region = configuracion.get('region')
+    subject = f'[{region}]Script automatizado de Openvas actualizado {version}'
+    message = f'"""<html>
+    <head></head>
+    <body>
+    <p>El script automatizado de openvas se ha actualizado con el siguiente resultado:</p>
+    <p>{resultado}</p>
+    </body>
+    </html>
+    """'
+    msg = MIMEMultipart()
+    msg['From'] = from_address
+    msg['To'] = to_address
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'html'))
+    smtp = smtplib.SMTP(smtp_server, smtp_port)
+    smtp.sendmail(from_address, to_address, msg.as_string())
+    smtp.quit()
+
 def get_version_github(url):
     try:
         response = requests.get(url)
@@ -16,9 +44,9 @@ def get_version_github(url):
     except Exception as e:
         print(f"Error: {e}")
         
-def leer_configuracion():
+def leer_configuracion(fichero):
     try:
-        with open('/home/redteam/gvm/Config/config_example.json', 'r') as archivo:
+        with open(fichero, 'r') as archivo:
             configuracion = json.load(archivo)
             return configuracion
     except FileNotFoundError:
@@ -37,4 +65,7 @@ if(version_github==version_local):
     print("Misma version")
 else:
     print("Diferente version")
-    subprocess.run(["git","pull"], cwd='/home/redteam/gvm/')
+    config = leer_configuracion('/home/redteam/gvm/Config/config.json')
+    resultado = subprocess.run(["git","pull"], cwd='/home/redteam/gvm/', capture_output=True, text=True)
+    email(version_github, config, resultado)
+    
